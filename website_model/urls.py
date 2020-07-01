19dca85f6cce49b1b2955651eb80c7db585e8d8e
FauzanWaseem786 <fauzan.ece17@nituk.ac.in>
2020-07-01 19:47:46 +0530

    initial commit

--- website_model/urls.py
+++ website_model/urls.py
@@ -2,7 +2,9 @@ from . import views
 from django.urls import path
 app_name='website_model'
 urlpatterns=[
-path('', views.index, name='index'),
+path('', views.index, name='index'),#Home page
+path('otp/',views.otp,name='otp'),#otp page
+path('Membership/',views.Membership,name='Membership'),#registration page
 path('Variety/',views.Variety,name='Variety'),
 path('brands/',views.brands,name='brands'),
 path('location/',views.location,name='location'),
@@ -11,11 +13,11 @@ path('shoes/',views.shoes,name='shoes'),
 path('sandals/',views.sandals,name='sandals'),
 path('boots/',views.boots,name='boots'),
 path('slippers/',views.slippers,name='slippers'),
-path('Membership/',views.Membership,name='Membership'),
 path('login/',views.login,name='login'),
 path('ilogout/',views.ilogout,name='ilogout'),
 path('order/',views.OrDer,name='OrDer'),
 path('change/',views.change,name='change'),
-
+path('broadcast_sms/',views.broadcast_sms,name='broadcast_sms'),
+path('reg/',views.reg,name='reg'),
 
 ]
--- website_model/views.py
+++ website_model/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render,redirect
-
+from django.conf import settings
+import random
+from twilio.rest import Client
+from django.views.decorators.csrf import csrf_exempt
+from twilio.twiml.messaging_response import MessagingResponse
 from django.contrib.auth import authenticate, login as dj_login, logout
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect, HttpResponse
@@ -12,6 +16,8 @@ from django.core.mail import send_mail
 # Create your views here.
 def index(request):
     return render(request,'website_model/home.html')
+def reg(request):
+    return render(request,'website_model/registered.html')
 def Premium(request):
     return render(request,'website_model/premium.html')
 def Variety(request):
@@ -29,6 +35,8 @@ def sandals(request):
 def slippers(request):
     return render(request,'website_model/slippers.html')
 def Membership(request):
+    global registered
+    global u1
     registered = False
     if(request.method == 'POST'):
         Member = Membr_form(request.POST,request.FILES)
@@ -36,6 +44,7 @@ def Membership(request):
         if Member.is_valid() :
             user = Member.save()
             username = request.POST.get('Name', '')
+            u1=username
             password = request.POST.get('password', '')
             Email = request.POST.get('email', '')
             Hometown=request.POST.get('Hometown','')
@@ -47,15 +56,16 @@ def Membership(request):
             if user is None:
                 # create user account and return the user object.
                 user = get_user_model().objects.create_user(username=username, password=password, email=Email)
-                registered = True
+
                 user.save()
 
         else:
             print("user_form.errors")
+        return redirect('/otp/')
     else:
         Member=Membr_form()
         #profile=pro_form()
-    return render(request,'website_model/premium.html',{ 'mem':Member,'registered':registered})
+        return render(request,'website_model/premium.html',{ 'mem':Member,'registered':registered})
 def login(request):
     logo=False
     if request.method == 'POST':
@@ -109,3 +119,34 @@ def OrDer(request):
         order = ord_form()
 
     return render(request,'website_model/order.html',{ 'order':order,'O':O})
+
+def broadcast_sms(request):
+    message_to_broadcast = ("Have you played the incredible TwilioQuest "
+                                                "yet? Grab it here: https://www.twilio.com/quest")
+    client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+    for recipient in settings.SMS_BROADCAST_TO_NUMBERS:
+        if recipient:
+            client.messages.create(to=recipient,
+                                   from_=settings.TWILIO_NUMBER,
+                                   body=message_to_broadcast)
+    return HttpResponse("messages sent!", 200)
+global no
+no=0
+def otp(request):
+    global no
+    if request.method == 'POST':
+        otp=request.POST.get('otp','')
+        if(int(otp)==int(no)):
+            return redirect('/reg/')
+        else:
+            u=Member.objects.filter(Name=u1)
+            u.delete()
+            us=User.objects.get(username=u1)
+            us.delete()
+            return HttpResponse('Invalid OTP.')
+
+    else:
+        no=1000
+        #no=random.randrange(1000,9999)
+        #send_mail('Your OTP for verification',' Your OTP is {}'.format(no),'vasufauzan786@gmail.com',['vasufauzan1010@gmail.com'],fail_silently=False)
+        return render(request, 'website_model/otp.html', {})
